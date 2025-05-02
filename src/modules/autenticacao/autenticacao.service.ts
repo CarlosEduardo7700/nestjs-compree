f3976304448dc64de5001b4a50f2ac44c7c863fd
CarlosEduardo7700 <carlos.msilva@quintoandar.com.br>
2025-05-02 20:46:19 -0300

    feat: Implementa o uso do JWT no login

--- src/modules/autenticacao/autenticacao.service.ts
+++ src/modules/autenticacao/autenticacao.service.ts
@@ -1,10 +1,19 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsuarioService } from '../usuario/usuario.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+interface UsuarioPayload {
+  sub: string;
+  nomeUsuario: string;
+}
 
 @Injectable()
 export class AutenticacaoService {
-  constructor(private usuarioService: UsuarioService) {}
+  constructor(
+    private usuarioService: UsuarioService,
+    private jwtService: JwtService,
+  ) {}
 
   async login(email: string, senha: string) {
     const usuario = await this.usuarioService.buscaPorEmail(email);
@@ -15,6 +24,13 @@ export class AutenticacaoService {
       throw new UnauthorizedException('Senha ou Email inválido!');
     }
 
-    console.log('Usuário autenticado!');
+    const payload: UsuarioPayload = {
+      sub: usuario.id,
+      nomeUsuario: usuario.nome,
+    };
+
+    return {
+      token_acesso: await this.jwtService.signAsync(payload),
+    };
   }
 }
