13e396b386c5ad2971beadae026c80b258676876
CarlosEduardo7700 <carlos.msilva@quintoandar.com.br>
2025-05-05 20:43:59 -0300

    feat: Configura o ConsoleLogger para logs de erros

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,8 @@
-import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  ConsoleLogger,
+  Module,
+} from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostgresConfigService } from './config/postgres.config.service';
@@ -40,6 +44,7 @@ import { AutenticacaoModule } from './modules/autenticacao/autenticacao.module';
       provide: APP_INTERCEPTOR,
       useClass: ClassSerializerInterceptor,
     },
+    ConsoleLogger,
   ],
 })
 export class AppModule {}
